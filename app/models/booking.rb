bd46d53db168677dd6f9cd7d0be7b128e50f12c2
VictoriaEtienne <victoria.etienne@outlook.fr>
2024-02-27 16:07:04 +0100

    Dashboardfinal

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :pony
   validates :start_time, presence: true
   validates :end_time, presence: true
+  enum status: { pending: 0, accepted: 1, declined: 2 }
 end
