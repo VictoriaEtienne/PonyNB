fd1d3cd732c20a0f8cdf5798c6aa2f04fa5dd799
VictoriaEtienne <victoria.etienne@outlook.fr>
2024-02-27 14:43:24 +0100

    Pagescontroller3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,11 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
+    @booked_ponies = @user.bookings.includes(:pony).map(&:pony)
+    @total_bookings = @user.bookings.count
+    @pending_bookings = @user.bookings.pending.count
+    @accepted_bookings = @user.bookings.accepted.count
+    @declined_bookings = @user.bookings.declined.count
   end
 
 end
