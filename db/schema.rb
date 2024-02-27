ed27672c387cf5a56afaab49b3e7c04f1832fc3c
antoine dempt <a.dempt@free.fr>
2024-02-27 14:17:27 +0100

    booking controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_150931) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_112705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.float "total_price"
-    t.string "start_time"
-    t.string "end_time"
     t.string "status"
     t.bigint "user_id", null: false
     t.bigint "pony_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_time"
+    t.date "end_time"
     t.index ["pony_id"], name: "index_bookings_on_pony_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
