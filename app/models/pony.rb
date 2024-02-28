7a392057a67271e766cb0fd4f9c685ccc2c212fb
VictoriaEtienne <victoria.etienne@outlook.fr>
2024-02-28 15:13:47 +0100

    AcceptDecline

--- app/models/pony.rb
+++ app/models/pony.rb
@@ -2,4 +2,5 @@ class Pony < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_095352) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_140449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.float "total_price"
-    t.integer "status"
+    t.string "status", default: "pending"
     t.bigint "user_id", null: false
     t.bigint "pony_id", null: false
     t.datetime "created_at", null: false
